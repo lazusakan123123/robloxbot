dda4ce51fbd1d516f3d0b2e4690f00be4332b397
lazusakan123123 <44501653+lazusakan123123@users.noreply.github.com>
2018-11-15 18:34:53 +0300

    Update davet.js

--- komutlar/davet.js
+++ komutlar/davet.js
@@ -13,7 +13,7 @@ exports.run = (client, message) => {
     .setColor(0x00AE86)
     .setTimestamp()
     .setAuthor(message.author.username, message.author.avatarURL)
-    .setDescription('İşte Davet Linkim: https://discordapp.com/oauth2/authorize?client_id=512296773004558337&scope=bot&permissions=268435612 <3 botumu eklersen sevinirim <3 :D <3');
+    .setDescription('İşte Davet Linkim: https://discordapp.com/oauth2/authorize?client_id=512649812647215114&scope=bot&permissions=168 <3 botumu eklersen sevinirim <3 :D <3');
     return message.author.sendEmbed(pingozel)
 };
 
